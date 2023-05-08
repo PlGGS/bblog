ac22f5e7c2edf2ef9cb8049c9abfe9242690c0ab
PlGGS <bbor0422@gmail.com>
2023-05-08 04:11:38 -0500

    Cleaned up prompt_name_age

--- worksheet4/prompt_name_age/src/main.rs
+++ worksheet4/prompt_name_age/src/main.rs
@@ -1,30 +1,32 @@
+use std::num::ParseIntError;
 use std::str::FromStr;
 use std::error::Error;
 use std::io::Write;
 
 fn main () {
-  let name: String = get_name("What is your name?\n> ").unwrap();
-  println! ("Hello {name}!");
-  let age: u32 = get_age("What is your age?\n> ").unwrap();
-  println!("You are {age} years old.");
-  println! ("Goodbye {name}!");
+    let name: String = get_from_stdin("name").unwrap();
+    println! ("Hello {name}!");
+
+    let age_string: String = get_from_stdin("age").unwrap();
+    let parsed_age: Result<u32, ParseIntError> = u32::from_str(age_string.trim());
+    if let Err(e) = parsed_age {
+        println!("{:?}", e);
+        return;
+    }
+    
+    let age: u32 = parsed_age.unwrap();
+    println!("You are {age} years old.");
+    println! ("Goodbye {name}!");
 }
 
-fn get_name(prompt: &str) -> Result<String, Box<dyn Error>> {
-    print! ("{}", prompt);
-    // print! ("> ");
-    std::io::stdout ().flush ()?;
-    let mut name : String = String::with_capacity (64);
-    let _num_bytes_read = std::io::stdin ().read_line (&mut name)?;
-    return Ok(name.trim().to_string());
-}
-
-fn get_age(prompt: &str) -> Result<u32, Box<dyn Error>> {
-    print! ("{}", prompt);
-    // print! ("> ");
-    std::io::stdout ().flush ()?;
-    let mut age : String = String::with_capacity (64);
-    let _num_bytes_read = std::io::stdin ().read_line (&mut age)?;
-    let parsed_age: u32 = u32::from_str(age.trim())?;
-    return Ok(parsed_age);
+fn get_from_stdin(val_to_get: &str) -> Result<String, Box<dyn Error>> {
+    let mut value : String = String::with_capacity (64);
+    
+    while value.trim().is_empty() {
+        print!("What is your {val_to_get}?\n> ");
+        std::io::stdout ().flush ()?;
+        std::io::stdin ().read_line (&mut value)?;
+    }
+    
+    return Ok(value.trim().to_string());
 }
