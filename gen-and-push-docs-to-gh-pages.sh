ed2ee87339d29ce26ae3095eadbcd6d5df8474fb
PlGGS <bbor0422@gmail.com>
2023-06-20 02:28:59 -0500

    Move docs to temp dir during move from main to gh-pages

--- gen-and-push-docs-to-gh-pages.sh
+++ gen-and-push-docs-to-gh-pages.sh
@@ -1,11 +1,12 @@
 git checkout main
 git pull origin main
 cargo doc
+cp -r target/doc ..
+rm -rf target/doc
 git checkout gh-pages
-git merge main --no-commit
-git checkout main -- target/doc
+cp -r ../doc target/doc
+rm -rf ../doc
 git add .
 git commit -m "Updated docs"
 git push origin gh-pages
 git checkout main
-rm -rf target
