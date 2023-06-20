9b5a26521ef9ee538766c006d73c06c9222c1594
PlGGS <bbor0422@gmail.com>
2023-06-20 01:50:08 -0500

    Updated doc generation script

--- gen-and-push-docs-to-gh-pages.sh
+++ gen-and-push-docs-to-gh-pages.sh
@@ -1,8 +1,10 @@
 git checkout main
-cargo doc
 git pull origin main
+cargo doc
 git checkout gh-pages
 git merge main --no-commit
 git checkout main -- target/doc
 git commit -m "Updated docs"
 git push origin gh-pages
+git checkout main
+rm -rf target
